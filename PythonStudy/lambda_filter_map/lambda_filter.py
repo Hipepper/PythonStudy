65a784579781bb468044621553bed998eaab8b90
7909213@qq.com <7909213@qq.com>
2017-01-31 13:37:13 +0800

    2.x and 3.x support

--- PythonStudy/lambda_filter_map/lambda_filter.py
+++ PythonStudy/lambda_filter_map/lambda_filter.py
@@ -1,7 +1,21 @@
+import sys
+
+
+def my_filter_print(result):
+    if sys.version_info.major == 3:
+        tmp = []
+        for item in result:
+            tmp.append(item)
+    else:
+        tmp = result
+    print(tmp)
+
+
 def lambda_filter():
     filter_me = [1, 2, 3, 4, 6, 7, 8, 11, 12, 14, 15, 19, 22]
     result = filter(lambda x: x % 2 == 0, filter_me)
-    print(result)
+    my_filter_print(result)
+
 
 if __name__ == "__main__":
     lambda_filter()
--- PythonStudy/lambda_filter_map/lambda_map.py
+++ PythonStudy/lambda_filter_map/lambda_map.py
@@ -1,7 +1,20 @@
+import sys
+
+
+def my_map_print(result):
+    if sys.version_info.major == 3:
+        tmp = []
+        for item in result:
+            tmp.append(item)
+    else:
+        tmp = result
+    print(tmp)
+
+
 def lambda_map():
     map_me = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
     result = map(lambda x: "the letter is %s" % x, map_me)
-    print(result)
+    my_map_print(result)
 
 
 if __name__ == "__main__":
