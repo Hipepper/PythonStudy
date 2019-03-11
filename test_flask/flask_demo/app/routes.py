4fd678631591fbbb2d634c5e02a5b2dc764dc236
glj <7909213@qq.com>
2019-03-11 18:38:07 +0800

    update some flask test code

--- test_flask/flask_demo/app/routes.py
+++ test_flask/flask_demo/app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -28,3 +29,16 @@ def login():
         flash('用户登录的名户名是:{} , 是否记住我:{}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='登 录', form=form)
+
+
+@app.route('/user/<username>')
+# @login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author':user,'body':'测试Post #1号'},
+        {'author':user,'body':'测试Post #2号'}
+    ]
+
+    return render_template('user.html',user=user,posts=posts)
+
---
+++ test_flask/flask_demo/testdb.py
@@ -0,0 +1,77 @@
+from app import db
+from app.models import User, Post
+
+
+def test_add_user(username, email):
+    try:
+        u = User(username=username, email=email)
+        db.session.add(u)
+        db.session.commit()
+        print("add user: %s success, email: %s" % (username, email))
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+
+
+def test_del_user(username=None):
+    users = User.query.all()
+    for u in users:
+        if username is None or u.username == username:
+            try:
+                db.session.delete(u)
+                db.session.commit()
+                print("del user: %s success" % (username))
+            except Exception as e:
+                print(e)
+                db.session.rollback()
+            return
+
+
+def test_query_user():
+    users = User.query.all()
+    for u in users:
+        print("user: %s, id: %d, email: %s" % (u.username, u.id, u.email))
+
+
+def test_add_post(body, author):
+    try:
+        p = Post(body=body,author=author)
+        db.session.add(p)
+        db.session.commit()
+        print("add post success, body: %s" % (body))
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+
+
+def test_del_post(body=None):
+    posts = Post.query.all()
+    for p in posts:
+        if body is None or p.body == body:
+            try:
+                db.session.delete(p)
+                db.session.commit()
+                print("del post success, body: %s" % (body))
+            except Exception as e:
+                print(e)
+                db.session.rollback()
+            return
+
+
+def test_query_post():
+    posts = Post.query.all()
+    for p in posts:
+        print("post body: %s, id: %d, user id: %s" % (p.body, p.id, p.user_id))
+
+
+if __name__ == '__main__':
+    test_add_user(username='glj', email='glj@163.com')
+    test_add_user(username='jrj', email='jrj@163.com')
+    test_query_user()
+    # test_del_user(username='glj')
+    # test_del_user(username='jrj')
+    u = User.query.get(1)
+    print(u)
+    # test_add_post(body='我第一次提交数据！',author=u)
+    # test_add_post(body='我第二次提交数据！',author=u)
+    test_query_post()
